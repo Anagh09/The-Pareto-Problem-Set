1767451f4e281052870bdfdd889af14e9f8f90d0
Anagh Saha <s19anagh@gmail.com>
2026-08-08 04:59:34 +0100

    Time: 0 ms (100.00%) | Memory: 20.5 MB (17.54%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,10 +1,24 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         prevMap = {} #val:index 
-        
+        #a one-pass hash table 
         for i, n in enumerate(nums):
             diff = target - n
             if diff in prevMap:
                 return [prevMap[diff], i]
             prevMap[n] = i
         return
+
+        """
+        both solutions have time and space complexity of O(n)
+        a two-pass hash table 
+         hashmap = {}
+        for i in range(len(nums)):
+            hashmap[nums[i]] = i
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in hashmap and hashmap[complement] != i:
+                return [i, hashmap[complement]]
+        # If no valid pair is found, return an empty list
+        return []
+        """
