83b4cca3994cdf91da165e4ddff2bc28812ea4a9
Anagh Saha <s19anagh@gmail.com>
2026-08-26 03:44:11 +0100

    Time: 4 ms (49.38%) | Memory: 20.6 MB (8.22%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -1,5 +1,19 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        l,r = 0, len(numbers)-1
+
+        while l<r:
+            curSum = numbers[l] + numbers[r]
+
+            if curSum > target:
+                r -= 1
+            elif curSum < target:
+                l += 1
+            else:
+                return [l+1 , r+1]
+        return []
+
+        """
         prevMap = {} #val:index 
         #a one-pass hash table 
         for i, n in enumerate(numbers):
@@ -8,3 +22,4 @@ class Solution:
                 return [prevMap[diff]+1, i+1]
             prevMap[n] = i
         return
+        """
